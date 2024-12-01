c6b5920f5139f23f906763104c34b0cde5bf4827
daynosX <awesome1155@outlook.com>
2024-12-01 11:28:08 -0500

    getCurrentlyPlaying errorhandling

--- app/api/spotify/tracks/getCurrentlyPlaying/route.js
+++ app/api/spotify/tracks/getCurrentlyPlaying/route.js
@@ -1,20 +1,39 @@
 import { getCurrentlyPlaying } from "@/app/lib/spotify"
 
-export async function GET(request){
+export async function GET(request) {
     // Gets the user's currently playing track in spotify. 
     let response = await getCurrentlyPlaying()
 
-    //Only take out the object we need and ignore everything else
-    let { item } = await response.json()
+    
+    try {
+        //Only take out the object we need and ignore everything else
+        let { item } = await response.json()
 
-
-    //Structure our response to things we need.
-    //Can be updated later if the need arises
-    let track = {
-        "title": item.name,
-        "album": item.album.name,
-        "artists": item.artists,
-        "duration": Math.floor(item.duration_ms / 60000) + ':' + Math.floor((item.duration_ms / 1000) % 60).toString().padStart(2, '0') //I hate long lines like this but if it works it works
+        //Structure our response to things we need.
+        //Can be updated later if the need arises
+        let track = {
+            "title": item.name,
+            "album": item.album.name,
+            "artists": getAllArtistNames(item.artists),
+            "duration": Math.floor(item.duration_ms / 60000) + ':' + Math.floor((item.duration_ms / 1000) % 60).toString().padStart(2, '0') // hate long lines like this but if it works it works
+        }
+        return Response.json(track)
+    } catch (error) {
+        return Response.json({"error": "No currently playing track!"})
     }
-    return Response.json(track)
+
+
+
+
+}
+
+function getAllArtistNames(artists) {
+
+    let artistArray = []
+
+    for (const element of artists) {
+        artistArray.push(element.name)
+    }
+
+    return artistArray
 }
